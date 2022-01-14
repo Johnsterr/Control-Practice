896f72c945bea850ebc869d668513c228cebb8d6
Evgeniy Pashko <evgpashko92@gmail.com>
2022-01-14 22:53:38 +0700

    Create update number function

--- ControlPractice/ViewController.swift
+++ ControlPractice/ViewController.swift
@@ -14,21 +14,39 @@ class ViewController: UIViewController {
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var textField: UITextField!
     
+    var number: UInt8 = 128
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
+    }
+    
+    /// Update all outlets to number
+    func updateUI() {
+        button.setTitle("\(number)", for: [])
+        
+        // TODO: set switches to number
+        
+        slider.value = Float(number)
+        textField.text = "\(number)"
     }
 
     @IBAction func buttonPressed() {
+        number = UInt8((Int(number) + 1) % 256)
+        updateUI()
     }
     
     @IBAction func switchToggled(_ sender: UISwitch) {
     }
     
     @IBAction func sliderMoved() {
+        number = UInt8(slider.value)
+        updateUI()
     }
     
     @IBAction func textFieldEdited() {
+        number = UInt8(textField.text ?? "") ?? 128
+        updateUI()
     }
     
 }
