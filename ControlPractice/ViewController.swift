536ec619eb80dcbc4d1773a694612cc4b9ed859e
Evgeniy Pashko <evgpashko92@gmail.com>
2022-01-15 13:09:22 +0700

    Implemented switches settings and getting

--- ControlPractice/ViewController.swift
+++ ControlPractice/ViewController.swift
@@ -18,15 +18,36 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        rotateSwitches()
         updateUI()
+    }
+    
+    func rotateSwitches() {
+        for `switch` in switches {
+            `switch`.layer.transform = CATransform3DMakeRotation(-.pi / 2, 0, 0, 1)
+        }
+    }
+    
+    /// Update number from the switches set
+    func updateNumberFromSwitches() {
+        var number = 0
+        for  `switch` in switches {
+            number += `switch`.isOn ? `switch`.tag : 0
+        }
+        self.number = UInt8(number % 256)
+    }
+    
+    /// Update switches from the number
+    func updateSwitches() {
+        for `switch` in switches {
+            `switch`.isOn = Int(number) & `switch`.tag != 0
+        }
     }
     
     /// Update all outlets to number
     func updateUI() {
         button.setTitle("\(number)", for: [])
-        
-        // TODO: set switches to number
-        
+        updateSwitches()
         slider.value = Float(number)
         textField.text = "\(number)"
     }
@@ -37,6 +58,8 @@ class ViewController: UIViewController {
     }
     
     @IBAction func switchToggled(_ sender: UISwitch) {
+        updateNumberFromSwitches()
+        updateUI()
     }
     
     @IBAction func sliderMoved() {
